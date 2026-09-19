87fd213cde23f4eb265cf88bd8bbc546ed5dabfb
4evy <git@evy.pink>
2026-09-19 11:10:00 +0300

    determinate: 3.22.4 -> 3.22.5

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       url = "github:4evy/browser";
     };
 
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/=3.22.4";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/=3.22.5";
 
     sops-nix.inputs.nixpkgs.follows = "";
     sops-nix.url = "github:Mic92/sops-nix";
